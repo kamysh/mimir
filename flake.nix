3cefb19e719f266f2646263c3b8d5e51a843ec4d
Valentyn Kamyshenko @ tartar <kamysh@kamysh.org>
2026-06-14 03:11:05 +0000

    chore(release): bump version to 0.11.0

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
         # Shared attrs for both packages
         commonAttrs = {
           pname = "mimir";
-          version = "0.10.0";
+          version = "0.11.0";
           src = ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
